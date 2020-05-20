fea24d184a846c62bdd66871f2fb70587143d8b6
shamlin143 <shamlin@gvtc.com>
2020-05-19 21:31:01 -0500

    routes

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
 });
 
 //Routes
-app.use(require("../WorkoutTracker/routes/api"));
-app.use(require("../WorkoutTracker/routes/html"));
+app.use(require("./routes/api"));
+app.use(require("./routes/html"));
 
 // Start the server
 app.listen(PORT, () => {
